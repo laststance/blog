5446eefddf4cfc7724e8cee4d6656570087e51f5
ryota-murakami <dojce1048@gmail.com>
2023-06-05 20:37:49 +0900

    implement getPost()

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,42 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { extract } from "$std/front_matter/any.ts";
+import { join } from "$std/path/mod.ts";
+import type { Handlers } from "$fresh/server.ts";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
-    </>
-  );
+interface Post {
+  slug: string;
+  title: string;
+  publishedAt: Date;
+  content: string;
+  snippet: string;
+}
+
+export const hander: Handlers<Post[]> = {
+  async GET(_req, ctx) {
+    const posts = await getPosts();
+    return ctx.render(posts);
+  },
+};
+
+async function getPosts(): Promise<Post[]> {
+  const files = Deno.readDir("./posts");
+  const promises = [];
+  for await (const file of files) {
+    const slug = file.name.replace(".md", "");
+    promises.push(getPost(slug));
+  }
+  const posts = await Promise.all(promises) as Post[];
+  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  return posts;
+}
+
+async function getPost(slug: Post["slug"]): Promise<Post | null> {
+  const text = await Deno.readTextFile(join("./posts", `${slug}.md`));
+  const { attrs, body } = extract<Partial<Post>>(text);
+  return {
+    slug,
+    title: attrs.title!,
+    publishedAt: new Date(attrs.publishedAt!),
+    content: body,
+    snippet: attrs.snippet!,
+  };
 }

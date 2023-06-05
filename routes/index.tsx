9b4e90cc93d3659c59c3d4ab06d5eb01f496e22e
ryota-murakami <dojce1048@gmail.com>
2023-06-05 21:05:28 +0900

    implement render postPage

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,3 +1,4 @@
+import type { PageProps } from "$fresh/server.ts";
 import { extract } from "$std/front_matter/any.ts";
 import { join } from "$std/path/mod.ts";
 import type { Handlers } from "$fresh/server.ts";
@@ -39,4 +40,35 @@ async function getPost(slug: Post["slug"]): Promise<Post | null> {
     content: body,
     snippet: attrs.snippet!,
   };
+}
+
+export default function BlogIndexPage(props: PageProps<Post[]>) {
+  const posts = props.data;
+  return (
+    <main class="max-w-screen-md px-4 pt-16 mx-auto">
+      <h1 class="text-5xl font-bold">Blog</h1>
+      <div class="mt-8">
+        {posts.map((post) => <PostCard post={post} />)}
+      </div>
+    </main>
+  );
+}
+
+function PostCard(props: { post: Post }) {
+  const { post } = props;
+  return (
+    <div class="py-8 border(t gray-200)">
+      <a class="sm:col-span-2" href={`/${post.slug}`}>
+        <h3 class="text(3xl gray-900) font-bold">{post.title}</h3>
+        <time class="text-gray-500">
+          {new Date(post.publishedAt).toLocaleString("en-us", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </time>
+        <div class="mt-4 text-gray-900">{post.snippet}</div>
+      </a>
+    </div>
+  );
 }

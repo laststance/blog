e59cbb069519c34f073db3cd070c53dc1b50f243
ryota-murakami <dojce1048@gmail.com>
2023-08-08 13:26:37 +0900

    change font

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,3 +1,4 @@
+import { Head } from '$fresh/runtime.ts'
 import { Handlers, PageProps } from '$fresh/server.ts'
 import { getPosts, Post } from '../utils/posts.ts'
 
@@ -5,37 +6,46 @@ export const handler: Handlers<Post[]> = {
   async GET(_req, ctx) {
     const posts = await getPosts()
     return ctx.render(posts)
-  },
+  }
 }
 
 export default function BlogIndexPage(props: PageProps<Post[]>) {
   const posts = props.data
   return (
-    <main class='max-w-screen-md px-4 pt-16 mx-auto'>
-      <h1 class='text-5xl font-bold'>Laststance Blog</h1>
-      <div class='mt-8'>
-        {posts.map((post) => <PostCard post={post} />)}
-      </div>
-    </main>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>Laststance.io Blog</title>
+        <meta name="description" content="publishing Laststance.io activity." />
+        <meta/>
+        <link rel="stylesheet" href="index.css"/>
+      </Head>
+      <main class="max-w-screen-md px-4 pt-16 mx-auto">
+        <h1 class="text-5xl font-bold">Laststance Blog</h1>
+        <div class="mt-8">
+          {posts.map((post) => <PostCard post={post}/>)}
+        </div>
+      </main>
+    </>
   )
 }
 
 function PostCard(props: { post: Post }) {
   const { post } = props
   return (
-    <div class='py-8 border(t gray-200)'>
-      <a class='sm:col-span-2' href={`/${post.slug}`}>
-        <h3 class='text(3xl gray-900) font-bold'>
+    <div class="py-8 border(t gray-200)">
+      <a class="sm:col-span-2" href={`/${post.slug}`}>
+        <h3 class="text(3xl gray-900) font-bold">
           {post.title}
         </h3>
-        <time class='text-gray-500'>
+        <time class="text-gray-500">
           {new Date(post.publishedAt).toLocaleDateString('en-us', {
             year: 'numeric',
             month: 'long',
-            day: 'numeric',
+            day: 'numeric'
           })}
         </time>
-        <div class='mt-4 text-gray-900'>
+        <div class="mt-4 text-gray-900">
           {post.snippet}
         </div>
       </a>

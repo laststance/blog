088fcdca99004dab3b64bc3a7f1ae7108a5b798f
ryota-murakami <dojce1048@gmail.com>
2023-08-12 14:29:13 +0900

    up

--- routes/index.tsx
+++ routes/index.tsx
@@ -21,7 +21,7 @@ export default function BlogIndexPage(props: PageProps<Post[]>) {
         <link rel="stylesheet" href="index.css"/>
       </Head>
       <main class="max-w-screen-md px-4 pt-16 mx-auto">
-        <h1 class="text-5xl font-bold">Laststance Blog</h1>
+        <h1 class="text-5xl font-bold">Laststance.io Blog</h1>
         <div class="mt-8">
           {posts.map((post) => <PostCard post={post}/>)}
         </div>
